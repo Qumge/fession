b83054338604e3981a5996b88e9964c7924c1bd3
heyen <heyen@Qumge-MBP2.local>
2019-12-10 18:56:42 +0800

    app登录

--- app/api/v1/entities/fission_log.rb
+++ app/api/v1/entities/fission_log.rb
@@ -4,9 +4,13 @@ module V1
       format_with(:timestamp) { |dt| dt.try :strftime, '%Y-%m-%d %H:%M:%S' }
       expose :id
       expose :token
+      expose :share_url do |instance, options|
+        URI::encode "#{Settings.h5_url}/pages/task/show?id=#{instance.task_id}&token=#{instance.token}"
+      end
       expose :parent, using: V1::Entities::FissionLog
       expose :user, using: V1::Entities::User
       expose :task, using: V1::Entities::Task
+
 
       # product_category 是在rails的model中定义的关联，在这里可以直接用
       #expose :company, using: V1::Entities::Company
--- app/models/user.rb
+++ app/models/user.rb
@@ -101,6 +101,11 @@ class User < ApplicationRecord
     end
   end
 
+  # @all
+  # 1.任务添加显示图
+  # 2. banner添加商户banner
+  # 3. app登录
+
 
   def ensure_authentication_token
     self.authentication_token ||= generate_authentication_token
