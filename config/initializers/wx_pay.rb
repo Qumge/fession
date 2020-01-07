1b90b51ce2a2176f510f165b69d40639ca6b57c8
heyen <heyen@Qumge-MBP2.local>
2020-01-07 19:31:35 +0800

    fix

--- config/initializers/wx_pay.rb
+++ config/initializers/wx_pay.rb
@@ -7,4 +7,4 @@ WxPay.key = '6A5AA25BDhzk8942FDF48ChzkAA964AC'
 #WxPay.set_apiclient_by_pkcs12(File.read(pkcs12_filepath), pass)
 
 WxPay.extra_rest_client_options = {timeout: 2, open_timeout: 3}
-WxPay.set_apiclient_by_pkcs12(File.read("/path/to/apiclient_cert.p12"), WxPay.mch_id)
+WxPay.set_apiclient_by_pkcs12(File.read("apiclient_cert.p12"), WxPay.mch_id)
